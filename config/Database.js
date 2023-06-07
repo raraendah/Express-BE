f0bad1e00360af14d1d2c42c316a8331f6520fa8
raraendah <rarandh5@gmail.com>
2023-06-07 13:02:10 +0700

    ubah db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('upload_db','root','',{
-    host: 'localhost',
+const db = new Sequelize('upload_db','admin','rara12345',{
+    host: 'db-express.cgdfqm7rvayu.ap-southeast-1.rds.amazonaws.com',
     dialect: "mysql"
 });
 

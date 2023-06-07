ea27a29cb47510a2d2906aa22f20e8ea948c326c
raraendah <rarandh5@gmail.com>
2023-06-07 16:47:28 +0700

    halaman halo

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,13 @@
 import {Sequelize} from "sequelize";
 
-// const db = new Sequelize('upload_db','admin','rara12345',{
-//     host: 'db-express.cgdfqm7rvayu.ap-southeast-1.rds.amazonaws.com',
-//     dialect: "mysql"
-// });
-
-const db = new Sequelize('upload_db','root','',{
-    host: 'localhost',
+const db = new Sequelize('upload_db','admin','rara12345',{
+    host: 'db-express.cgdfqm7rvayu.ap-southeast-1.rds.amazonaws.com',
     dialect: "mysql"
 });
+
+// const db = new Sequelize('upload_db','root','',{
+//     host: 'localhost',
+//     dialect: "mysql"
+// });
 
 export default db;
